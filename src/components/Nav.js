ba8f4eaee0e156e989360672f0bc352e2a5c4d7e
Auddity <audsecondarylap@outlook.com>
2022-09-16 14:41:52 -0400

    custom hook for responsive rendering

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
+import useWindowSize from '../hooks/useWindowSize';
 
 const Nav = () => {
+  const { width } = useWindowSize();
+
   return (
     <nav className="Nav">
-      <Link to="/portfolio">Portfolio</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      {width < 768 ?
+        <div className="bar-icon">
+        BARS
+        </div>
+      :
+        <div className="link-container">
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      }
     </nav>
   )
 }
