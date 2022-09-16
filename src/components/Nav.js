448ed234dc0fa49bfd563695e545735f65aef4f8
Auddity <audsecondarylap@outlook.com>
2022-09-16 16:56:16 -0400

    menu icon handler

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import useWindowSize from '../hooks/useWindowSize';
 import 'remixicon/fonts/remixicon.css'
 import '../scss/Nav.scss'
 
 const Nav = () => {
   const { width } = useWindowSize();
+  const [open, setOpen] = useState(false);
+  const rotateBars = {
+    transform: 'rotate(-90deg)',
+  }
+  const handleMenu = () => {
+    setOpen(prev => !prev)
+  }
 
   return (
-    <nav className="Nav">
+    <nav className={open ? "Nav showNav" : "Nav"}>
       {width < 768 ?
-        <div className="bar-icon">
-          <i className="ri-menu-line"></i>
+        <div className="bar-icon"
+          style={open ? rotateBars : null}
+          onClick={handleMenu}
+        >
+          <i className="ri-menu-line" ></i>
         </div>
       :
-        <div className="link-container">
+        <div className="link-container"
+        >
           <Link to="/portfolio">Portfolio</Link>
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
