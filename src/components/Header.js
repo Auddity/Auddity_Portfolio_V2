ab783b68adbcbe32f8547c8be4a1bf38f3b7d15d
Auddity <audsecondarylap@outlook.com>
2022-09-16 13:45:21 -0400

    font trial, header positioning

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import '../scss/Header.scss'
 import { Link } from 'react-router-dom';
-import logo from '../images/Auddity Logo 2013.png'
+// import logo from '../images/Auddity Logo 2013.png'
 import Nav from './Nav';
 
 const Header = () => {
@@ -9,7 +9,8 @@ const Header = () => {
     <header className="Header">
       <div className="logo-container">
         <Link to="/">
-          <img src={logo} alt="Auddity Logo" />
+          {/* <img src={logo} alt="Auddity Logo" /> */}
+          <p>Home</p>
         </Link>
       </div>
       Header
