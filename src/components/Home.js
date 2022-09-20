59b69f7b95e448266bf1a28eeaa4ea0f8f84f7dc
Auddity <audsecondarylap@outlook.com>
2022-09-20 19:30:07 -0400

    hero content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import '../scss/_Hero.scss'
+import logo from '../images/Auddity Logo 2013.png'
 
 const Home = () => {
   return (
     <main className="Hero">
-      
+      <div className="Hero-logo-ctnr">
+        <img src={logo} alt="Auddity Logo" />
+      </div>
+      <div className="Hero-info-ctnr">
+        <h1>Front End Develompent</h1>
+        <div className="tech-list">
+          <p>HTML</p>
+          <p>CSS</p>
+          <p>SASS</p>
+          <p>JAVASCRIPT</p>
+          <p>REACT</p>
+        </div>
+      </div>
     </main>
   )
 }
