4f0e25ad72d6cfe9c14f7d7871632367e5de9496
Auddity <audsecondarylap@outlook.com>
2022-09-29 14:42:00 -0400

    portfolio page component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,38 @@
+import portfolioData from '../data/portfolioData.json'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
 const Portfolio = () => {
+  const [portData, setPortData] = useState({});
+  
+
+  useEffect(() => {
+    const fetchPortData = async () => {
+      try {
+        const data = await (portfolioData)
+        setPortData(data.projects)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    fetchPortData();
+  }, [])
+  
+  console.log(portData)
+
   return (
-    <>
-      Portfolio
-    </>
+    <main className='Portfolio'>
+      {portData.map(item => {
+        return <div 
+          className="port-item-link"
+          key={item.id}
+        >
+          {item.title}
+          {item.id}
+        </div>
+      })}
+    </main>
   )
 }
 
