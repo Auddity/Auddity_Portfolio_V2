429335fcf2291fd4991b2c070c22b61a7e4249c8
Auddity <audsecondarylap@outlook.com>
2022-09-30 15:26:25 -0400

    map through images and data working

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,31 +1,31 @@
 import portfolioData from '../data/portfolioData.json'
+import img1 from '../images/SplitterAppScreenShot.png'
+import img2 from '../images/TeamStatTrackerApp ScreenShot 2022-02-08.png'
+import img3 from '../images/Shortly Screenshot 2022-02-08.png'
+import img4 from '../images/Spinner-ScreenShot.png'
+import img5 from '../images/crowdfund-fem-screenshot.png'
 import { Link } from 'react-router-dom'
-import img from '../images/SplitterAppScreenShot.png'
 import '../scss/_Portfolio.scss'
-
 
 
 const Portfolio = () => {
   const portData = portfolioData.projects;
+  const imageArray = [img1, img2, img3, img4, img5];
 
   return (
     <main className='Portfolio'>
       <div className="Portfolio-link-container">
-        {portData.map(item => {
-          // {console.log(item.image)}
+        {portData.map((item, idx)=> {
           return (
-
             <Link 
             className="port-item-link"
             key={item.id}
             to={`portfolio/${item.id}`}
             >
               <div className="port-item-link-content">
-                <img src={item.image} alt={item.title} />
-                
+                <img src={imageArray[idx]} alt={item.title} />  
               </div>
             </Link>
-          
           )
         })}
       </div>
