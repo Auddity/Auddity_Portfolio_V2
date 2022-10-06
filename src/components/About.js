996f41920dcf23056ce4a6c9ea8f5046ff31ba70
Auddity <audsecondarylap@outlook.com>
2022-10-06 13:01:15 -0400

    contact link hover effect

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import '../scss/_About.scss';
 import logo from '../images/Auddity Logo 2013.png'
-import alt_logo from '../images/Auddity Wave Logo Source.jpg'
+import wave_logo from '../images/Auddity Wave Logo Source.jpg'
 
 const About = () => {
   return (
@@ -10,10 +10,17 @@ const About = () => {
         <img src={logo} alt="Auddity Logo" />
       </div>
       <div className="About-info-container">
-        <img src={alt_logo} alt="Alternate Logo" />
+        <div className="wave-logo-container">
+          <img src={wave_logo} alt="Auddity Sound Wave Logo" />
+        </div>
         <p>My name is James Bretz and for the past two years I've been teaching myself front end development on a part-time basis.  I'm eager for full-time opportunities to apply my skills and grow my knowledge of front end development. Thank you for visiting my website and feel free to contact me.</p>
       </div>
-      <Link to={'/contact'}></Link>
+      <Link to={'/contact'}>
+        <i className='ri-send-plane-fill'></i>
+        <span>
+          Get in touch  
+        </span>
+      </Link>
     </main>
   )
 }
