b4dabb78df2fc3ee58f24625f9bd5f3abb395901
Auddity <audsecondarylap@outlook.com>
2022-10-06 15:50:01 -0400

    about responsive alignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,23 @@
 import { Link } from 'react-router-dom'
+import useWindowSize from '../hooks/useWindowSize';
 import '../scss/_About.scss';
 import logo from '../images/Auddity Logo 2013.png'
 import wave_logo from '../images/Auddity Wave Logo Source.jpg'
 
 const About = () => {
+  const { width } = useWindowSize();
+
   return (
     <main className="About">
       <div className="About-logo-container">
         <img src={logo} alt="Auddity Logo" />
       </div>
       <div className="About-info-container">
-        <div className="wave-logo-container">
-          <img src={wave_logo} alt="Auddity Sound Wave Logo" />
-        </div>
+        {width > 1028 && 
+          <div className="wave-logo-container">
+            <img src={wave_logo} alt="Auddity Sound Wave Logo" />
+          </div>
+        }
         <p>My name is James Bretz and for the past two years I've been teaching myself front end development on a part-time basis.  I'm eager for full-time opportunities to apply my skills and grow my knowledge of front end development. Thank you for visiting my website and feel free to contact me.</p>
       </div>
 
